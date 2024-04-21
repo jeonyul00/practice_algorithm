aa63f83a01428917db77fda4441e29939f3bb516
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2024-04-21 19:52:50 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.19 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ Swift/프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.swift
@@ -0,0 +1,17 @@
+func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
+    
+    var answer:[Int] = []
+ 
+    for i in arr {
+        if i % divisor == 0 {
+            answer.append(i)
+        }        
+    }
+    
+    if answer.count == 0 {
+        return [-1]    
+    } else {
+        return answer.sorted()
+    }
+    
+}
