ed01dee89e3dc35f34f7d33d691f0391862bb2e9
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2024-10-02 00:43:16 +0900

    [level 1] Title: 소수 찾기, Time: 582.63 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.swift
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.swift
@@ -3,11 +3,16 @@ import Foundation
 func solution(_ n: Int) -> Int {
     guard n > 1 else { return 0 }
         
+    
+    if n == 2 {
+        return 1
+    }
+
     var isPrime = Array(repeating: true, count: n + 1)
     isPrime[0] = false 
     isPrime[1] = false
     
-    for i in 2...n {
+    for i in 2...Int(sqrt(Double(n))) {
         if isPrime[i] {
             for multiple in stride(from: i * i, through: n, by: i) {
                 isPrime[multiple] = false
