3fc39fe5a51d3de423a8dcff4520be5b2c89ba2e
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2024-10-01 19:57:58 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 3.96 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
@@ -0,0 +1,14 @@
+func solution(_ strings: [String], _ n: Int) -> [String] {
+    let sortedStrings = strings.sorted { 
+        let firstChar = $0[$0.index($0.startIndex, offsetBy: n)]
+        let secondChar = $1[$1.index($1.startIndex, offsetBy: n)]
+        
+        if firstChar == secondChar {
+            return $0 < $1
+        } else {
+            return firstChar < secondChar
+        }
+    }
+    
+    return sortedStrings
+}
