804fb86bf546d9486e85e3cc5481266496421d46
jeonyul <jeonyul00@gmail.com>
2024-10-03 16:26:43 +0900

    update

--- Independent/15.연속부분수열1.playground/Contents.swift
+++ Independent/15.연속부분수열1.playground/Contents.swift
@@ -1,24 +1,27 @@
 import UIKit
 
-func solution(_ num:Int, _ arr:[Int]) -> Int {
+func solution(_ num: Int, _ arr: [Int]) -> Int {
     var answer: Int = 0
     var startPoint: Int = 0
-    var endPoint: Int = 0
     var sum: Int = 0
     
-    
-    // num보다 작으면 endPoint 증가, 크면 startPoint가 증가
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // endPoint는 배열을 순회하는 포인터
+    for endPoint in 0..<arr.count {
+        sum += arr[endPoint]
         
+        // 구간 합이 num보다 크거나 같으면 startPoint를 이동시킴
+        while sum > num {
+            sum -= arr[startPoint]
+            startPoint += 1
+        }
+        
+        // 구간 합이 num과 같으면 카운트 증가
+        if sum == num {
+            answer += 1
+        }
+    }
+    
     return answer
 }
 
-print(solution(6,[1,2,1,3,1,1,1,2]))
+print(solution(6, [1, 2, 1, 3, 1, 1, 1, 2]))  // 출력: 3
