0ad7645375f0306db2fad0ada071ba6cf407bcba
jeonyul <jeonyul00@gmail.com>
2024-10-07 19:04:30 +0900

    update

--- Independent/16.재귀.playground/Contents.swift
+++ Independent/16.재귀.playground/Contents.swift
@@ -2,9 +2,11 @@ import UIKit
 
 
 func DFS(_ num: Int) {
+    
     if num == 0 {
         return
     }
+    
     print(num) // 3, 2, 1
     DFS(num - 1)
     print(num) // 1, 2, 3
@@ -14,5 +16,21 @@ func solution(_ num: Int) {
     DFS(num)
 }
 
-solution(3)
+//solution(3)
 
+
+// --
+func recursion(_ num: Int) -> String {
+    
+    guard num > 0 else { return "이진수는 -----> " }        
+    return recursion(num / 2) + String(num % 2)
+    
+}
+
+// 재귀 이진수 출력
+func solution2(_ num:Int) -> String {
+    if num == 0 { return "0" }
+    return recursion(num)
+}
+
+print(solution2(11))
