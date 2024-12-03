06cdd37831846bc9b1947adca258e9a342f4724c
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2024-12-03 23:56:32 +0900

    [level 1] Title: 약수의 합, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,9 @@
 function solution(n) {
-    var answer = 0;
-    for (let i = 0; i <= n; i++) {
-    if (n % i === 0) {
-      answer += i;
+    let answer = 0;
+    for(let i=0; i <= n; i++){
+        if(n % i === 0){
+            answer += i
+        }
     }
-  }
-    return answer;
+    return answer
 }
