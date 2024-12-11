30e92d19b40153e2b85ecbd57f32d960ac494db9
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2024-12-11 13:19:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 40.05 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    const answer = [];    
+    for(let i=0; i < arr.length; i++){
+        if(arr[i] !== answer[answer.length - 1]){
+            answer.push(arr[i])
+        }
+    }
+    return answer;
+}
