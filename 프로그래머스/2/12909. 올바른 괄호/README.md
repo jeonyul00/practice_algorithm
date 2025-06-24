5548549d50c411bf2c7ec862442e4805c297a282
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2025-06-24 22:12:58 +0900

    [level 2] Title: 올바른 괄호, Time: 31.53 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 37.3 MB, 시간: 31.14 ms
+메모리: 37.5 MB, 시간: 31.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 04일 00:04:06
+2025년 06월 24일 22:12:54
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,16 +1,14 @@
 function solution(s){
-    var answer = true;
-    let arr = []
-
-    for(let i=0; i < s.length; i++){
-        if (s[i] === "(") {
-            arr.push("(")
-        } else if (s[i] === ")") {
-            if (arr.length === 0) {
+    const arr = []
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === '(') {
+            arr.push(s[i])
+        } else if(s[i] === ')'){
+            if(arr.length === 0){
                 return false
             } else {
                 arr.pop()
-            }
+            }            
         }
     }
     return arr.length === 0
