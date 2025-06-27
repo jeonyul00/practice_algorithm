1bb4f3bba8337c2d6c17ced162ae682a7cc49ef2
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2025-06-28 05:26:58 +0900

    [level 2] Title: 소수 찾기, Time: 4.73 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,36 @@
+
+
+function solution(numbers) {
+    const nums = numbers.split("")
+    const candidates = new Set();
+    let answer = 0
+    
+    function permute(arr,cur) {
+        if(cur.length > 0) {
+            candidates.add(Number(cur))
+        }
+        if (arr.length === 0) return;
+        for (let i = 0; i < arr.length; i++) {
+            const nextArr = arr.slice();            
+            const [digit] = nextArr.splice(i, 1);
+            permute(nextArr, cur + digit);
+        }
+    }
+    
+    function isPrime(num) {
+        if(num < 2) return false        
+        for(let i = 2; i <= Math.sqrt(num); i++){
+            if(num % i === 0) return false
+        }
+        return true        
+    }
+        
+    permute(nums, '');
+    
+    for(num of candidates) {        
+        if(isPrime(num)) answer++    
+    }
+    
+    return answer;
+}
+
