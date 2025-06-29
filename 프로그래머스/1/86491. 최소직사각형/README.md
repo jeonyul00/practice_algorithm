a9784105c40a5386ab74dc29a2f9e21e1a1aa632
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2025-06-29 16:23:05 +0900

    [level 1] Title: 최소직사각형, Time: 8.66 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 38.6 MB, 시간: 296.94 ms
+메모리: 38.9 MB, 시간: 8.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 24일 22:26:44
+2025년 06월 29일 16:23:01
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,11 +1,14 @@
+// 가로든 세로든 더 긴 녀석을 0번 인덱스로 몰아
+// 그런 후 제일 큰 가로 * 제일 큰 세로 리턴 때려
+
 function solution(sizes) {    
-    let maxW = -Infinity
-    let maxH = -Infinity
-    for (let [w, h] of sizes) {
-        const width = Math.max(w, h);
-        const height = Math.min(w, h);
-        maxW = Math.max(maxW, width);
-        maxH = Math.max(maxH, height);
-    }
-    return maxW * maxH;
+    let maxW = 0
+    let maxH = 0    
+    for(const [w,h] of sizes) {
+        const big = Math.max(w,h)
+        const small = Math.min(w,h)
+        maxW = Math.max(maxW, big)
+        maxH = Math.max(maxH, small)        
+    }        
+    return maxW * maxH    
 }
