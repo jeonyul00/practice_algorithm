5884e15e22876d50227f06f223a5eac2c3313381
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2025-06-29 16:51:31 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,22 +1,30 @@
 function solution(begin, target, words) {
-    if (!words.includes(target)) return 0
-
-    let queue = [[begin, 0]]
+    var answer = Number.MAX_SAFE_INTEGER;
+    const check = Array.from({length: words.length}).fill(false)        
     
-    while (queue.length > 0) {
-        let [currentWord, step] = queue.shift()
-
-        if (currentWord === target) return step
-
-        words.forEach((word, index) => {
-            let diff = 0
-            for (let i = 0; i < word.length; i++) {
-                if (word[i] !== currentWord[i]) diff++
+    const isCheckWord = (a,b) => {
+        let dif = 0
+        for(let i = 0; i < a.length; i++) {
+            if(a[i] !== b[i]) dif++
+        }        
+        return dif === 1        
+    }        
+    
+    function DFS(current, count) {
+        if(current === target){            
+            answer = Math.min(answer, count)
+        } else {
+            for(let i = 0; i < words.length; i++) {
+                if(!check[i] && isCheckWord(current, words[i])){
+                    check[i] = true
+                    DFS(words[i], count + 1)
+                    check[i] = false
+                }
             }
-            if (diff === 1) {
-                queue.push([word, step + 1])
-                words.splice(index, 1)
-            }
-        });
-    }    
+        }
+    }            
+    
+    DFS(begin, 0)                    
+    
+    return answer === Number.MAX_SAFE_INTEGER ? 0 : answer;
 }
