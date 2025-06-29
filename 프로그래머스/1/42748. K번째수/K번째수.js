0b89cf20090384398d9b68ed1d7e0fb877fc289a
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2025-06-29 16:14:07 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {    
+    var answer = [];
+    
+    for(element of commands) {
+        const arr = array.slice(element[0] - 1, element[1])
+        const sortedArr = arr.sort((a,b) => a - b)        
+        answer.push(sortedArr[element[2] - 1])
+        
+    }
+    
+    return answer;
+}
