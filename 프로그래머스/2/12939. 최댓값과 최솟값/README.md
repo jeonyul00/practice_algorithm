d3b92aef3a50c0885b32cb4f6cb281704fde0464
jeonyul <90760450+jeonyul00@users.noreply.github.com>
2025-09-25 12:41:39 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.32 ms
+메모리: 33.4 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 30일 11:15:35
+2025년 09월 25일 12:41:35
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,14 +1,12 @@
 function solution(s) {
+    const answer = s.split(" ");
     let max = Number.MIN_SAFE_INTEGER
     let min = Number.MAX_SAFE_INTEGER
-    let answer = '';
-    const arr = s.split(" ")
     
-    for(str of arr) {
-        max = Math.max(str, max)
-        min = Math.min(str, min)
+    for(let num of answer) {
+        min = Math.min(min, num)
+        max = Math.max(max, num)
     }
     
-    answer = min + " " + max
-    return answer;
+    return min + " " + max;
 }
