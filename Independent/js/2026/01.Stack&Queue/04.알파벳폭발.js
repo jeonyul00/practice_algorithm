ff51442a8cac58d226acd32252f02a8f09e9f5b7
jeonyul <jeonyul00@gmail.com>
2025-11-02 15:54:31 +0900

    update

--- Independent/js/2026/01.Stack&Queue/04.알파벳폭발.js
+++ Independent/js/2026/01.Stack&Queue/04.알파벳폭발.js
@@ -17,3 +17,26 @@ console.log(mySolution("aAbBcC"));
 console.log(mySolution("xYyX"));
 console.log(mySolution("a"));
 console.log(mySolution("Code"));
+
+function solution(s) {
+  const stack = [];
+
+  for (const char of s) {
+    if (
+      stack.length > 0 &&
+      stack[stack.length - 1].toLowerCase() === char.toLowerCase()
+    ) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.join("");
+}
+
+console.log(solution("infFflearn"));
+console.log(solution("aAbBcC"));
+console.log(solution("xYyX"));
+console.log(solution("a"));
+console.log(solution("Code"));
